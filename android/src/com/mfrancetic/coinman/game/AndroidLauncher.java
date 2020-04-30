a8a6026a1d41dfe037d1e8b7235bda00dae5d3a7
Maja Francetic <francetic.maja@gmail.com>
2020-04-30 13:30:25 +0200

    Initial commit

---
+++ android/src/com/mfrancetic/coinman/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mfrancetic.coinman.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mfrancetic.coinman.game.CoinMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMan(), config);
+	}
+}
